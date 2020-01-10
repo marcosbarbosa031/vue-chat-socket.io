173e199ad007a949ca23445e34d2a3d17adaba72
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-10 17:07:07 -0300

    Creating socket server logic

--- server.js
+++ server.js
@@ -4,13 +4,43 @@ var io = require('socket.io')(http);
 var path = require('path')
 var serveStatic = require('serve-static')
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+var users = [];
 
 io.on('connection', function (socket) {
-  socket.on('chatMessage', function (msg) {
-    console.log(`${msg.userId}: `, msg.message);
+  socket.on('join', user => {
+    if (!users.some(u => u.userId === user.userId)) {
+      console.log(`${user.username} joined the chat`)
+      socket.broadcast.emit('chatMessage', {
+        message: `${user.username} joined the chat`,
+        server: true
+      });
+      users.push(user);
+      
+      users.forEach(u => {
+        console.log('Users: ', u.username, '   Id: ', u.userId);
+      })
+    }
+  });
+
+  socket.on('chatMessage', msg => {
+    console.log(`${msg.username}: `, msg.message);
     io.emit('chatMessage', msg);
   });
+
+  // socket.on('disconnect', reason => {
+  //   console.log(`User disconnected. Motive: ${reason}`);
+  //   io.emit('userDisconnected');
+  // })
+
+  socket.on('leave', user => {
+    console.log(`${user.username} left the chat.`);
+    socket.broadcast.emit('userDisconnected', {
+      message: `${user.username} left the chat.`,
+      server: true
+    })
+  })
 });
 
 http.listen(4113, function () {
