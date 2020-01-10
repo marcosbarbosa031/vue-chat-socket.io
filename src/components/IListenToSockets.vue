91277189c60aff35d385150d43074f97462c7324
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-10 17:09:01 -0300

    Socket logic on client

--- src/components/IListenToSockets.vue
+++ src/components/IListenToSockets.vue
@@ -1,67 +1,98 @@
 <template>
   <div>
-    <p v-if="isConnected">We're connected to the server!</p>
-    <p v-for="message in socketMessage" :key="message.id">{{message.userId}}: {{message.message}}</p>
+    <div v-for="message in userMessages" :key="message.id">
+      <div v-if="message.server">
+        <p>{{ message.message | capitalize }}</p>
+      </div>
+      <div v-if="!message.server">
+        <p>{{ message.username | capitalize }}: {{ message.message }}</p>
+      </div>
+    </div>
     <form action="" @submit.prevent="sendMessage">
-      <input type="text" name="message" v-model="inputText">
-      <button >Send</button>
+      <input type="text" name="message" v-model="inputText" />
+      <button>Send</button>
     </form>
   </div>
 </template>
 
 <script>
-import { uuid } from 'vue-uuid'
-import socketio from 'socket.io-client'
+import socketio from "socket.io-client";
+import { uuid } from "vue-uuid";
 
 export default {
   data() {
     return {
       isConnected: false,
-      socketMessage: [],
-      inputText: '',
-      userId: '',
+      userMessages: [],
+      inputText: "",
+      userId: "",
+      username: "",
       socket: null
-    }
+    };
   },
 
   methods: {
-    connectSocker () {
-      let socket = socketio('http://localhost:4113')
-      this.socket = socket
+    connectSocker() {
+      let socket = socketio("http://localhost:4113");
+      this.socket = socket;
 
-      let data = {
-        session: 'dad7f362-2c09-4398-83a7-9529e44f407d',
-        name: 'Eu carai'
-      }
-
-      this.socket.emit('join', data)
-
+      let user = {
+        userId: this.userId,
+        username: this.username
+      };
       this.isConnected = true;
 
-      this.socket.on('chatMessage', msg => {
-        console.log('msg: ', msg)
-        console.log('socketMessage: ', this.socketMessage)
-        this.socketMessage.push(msg)
-      })
+      window.onbeforeunload = () => {
+        this.socket.emit('leave', user);
+      }
 
+      this.socket.emit("join", user);
+
+      this.socket.on("chatMessage", message => {
+        console.log("message: ", message);
+        this.userMessages.push(message);
+      });
+
+      this.socket.on('userDisconnected', message => {
+        console.log("message: ", message);
+        this.userMessages.push(message);
+      })
     },
+
     sendMessage() {
       let data = {
-        userId: this.userId,
-        message: this.inputText
+        username: this.username,
+        message: this.inputText,
+        server: false
+      };
+      console.log("data: ", data);
+      this.socket.emit("chatMessage", data);
+      this.inputText = "";
+    },
+
+    checkLocalStorage() {
+      if (this.$store.getters.getUsername) {
+        this.username = this.$store.getters.getUsername;
+        this.userId = this.$store.getters.getUserId;
+      } else {
+        this.$router.push({ path: "/" });
       }
-      this.socket.emit('chatMessage', data)
-      this.inputText = ''
+    },
+
+    generateUuid() {
+      this.userId = uuid.v4();
+      this.$store.dispatch("set_user_id", this.userId);
+      console.log("userId :", this.userId);
     }
   },
 
-  beforeMount () {
-    this.userId = uuid.v4()
-    console.log('userId :', this.userId)
+  beforeMount() {
+    this.generateUuid();
+    this.checkLocalStorage();
   },
 
-  mounted () {
-    this.connectSocker()
-  }
-}
+  mounted() {
+    this.connectSocker();
+  },
+};
 </script>
